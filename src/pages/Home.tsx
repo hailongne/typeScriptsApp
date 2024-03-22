896782a835235f22b80ad06b4b8a041a667f36bb
hailong <nongthanthien2004@gmail.com>
2024-03-22 13:21:06 +0700

    lesson 8

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
-import ProductList from '@/components/ProductList'
+import Banner from '@/components/Banner/Banner'
+import Footer from '@/components/Footer/Footer'
+import { Header } from '@/components/Header/Header'
+import ProductList from '@/components/ProductList/ProductList'
 
 type Props = {}
 const Home = (props: Props) => {
   return (
     <div>
+      <Banner />
       <h2>Danh sach san pham:</h2>
       <ProductList />
     </div>
